67e737c375944e032e3148f403b6ffdf466a83b5
comp-jihun7056 <comp.jihun7056@gmail.com>
2023-11-29 15:46:32 +0900

    코테문제

---
+++ src/com/algorithm/ex/queue/Prob2164.java
@@ -0,0 +1,23 @@
+package com.algorithm.ex.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Prob2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        int N = sc.nextInt();
+
+        for(int i=1; i<=N; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
---
+++ src/com/algorithm/ex/stack/Prob17298.java
@@ -0,0 +1,41 @@
+package com.algorithm.ex.stack;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Prob17298 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int[] A = new int[n];
+        int[] ans = new int[n];
+        String[] str = bf.readLine().split(" ");
+
+        for(int i=0; i<n; i++) {
+            A[i] = Integer.parseInt(str[i]);
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for(int i=1; i<n; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                ans[stack.pop()] = A[i];
+            }
+            stack.push(i);
+        }
+
+        while (!stack.empty()) {
+            ans[stack.pop()] = -1;
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i=0; i<n; i++) {
+            bw.write(ans[i] + " ");
+        }
+        bw.write("\n");
+        bw.flush();
+
+    }
+}
